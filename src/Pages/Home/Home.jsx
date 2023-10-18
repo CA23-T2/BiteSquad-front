5f6e302cfa7ec8e3251a1376ebd46137dc365364
Vuja <djordjijevujovic210306@gmail.com>
2023-10-18 21:20:58 +0200

    inital

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,80 +4,146 @@ import message from "../../Message.svg"
 import notifi from "../../Notification.svg";
 import users from "../../2User.svg";
 import BiteSquad from "../../BiteSquad.svg"
+import trash from "../../Trash.svg"
 import "./Home.scss";
 function App() {
+
+
+    const arr = [1,2];
+    arr.push(6);
+    arr.splice(1)
+    console.log(arr)
     return (
         <>
             <header>
                 <img src={BiteSquad} alt="" />
+                <div id="search">
+                    <input type="text" name="" id="" placeholder="Pretrazi hranu"/>
+                </div>
             </header>
-            <nav>
-                <div className="active">
-                    <img src={home} alt="" />
-                </div>
-                <div>
-                    <img src={message} alt="" />
-                </div>
-                <div>
-                    <img src={notifi} alt="" />
-                </div>
-                <div>
-                    <img src={users} alt="" />
-                </div>
-                <div>
-                    <img src={settings} alt="" />
-                </div>
-            </nav>
-            <section>
-                <div className="filter">
+            <section className="container">
+
+
+                <nav>
                     <div className="active">
-                        <p>Topla Jela</p>
+                        <img src={home} alt="" />
                     </div>
                     <div>
-                        <p>Topla Jela</p>
+                        <img src={message} alt="" />
                     </div>
                     <div>
-                        <p>Topla Jela</p>
+                        <img src={notifi} alt="" />
                     </div>
                     <div>
-                        <p>Topla Jela</p>
+                        <img src={users} alt="" />
                     </div>
                     <div>
-                        <p>Topla Jela</p>
-                    </div>  
-                </div>
-                <h2>Odabir jela</h2>
-                <div className="Food">
-                    <div>
-                        <img src={require("../../burger.png")} alt="" />
-                        <h3>Dupli  cheeseburge</h3>
-                        <p>06h-08h</p>
+                        <img src={settings} alt="" />
                     </div>
-                    <div>
-                        <img src={require("../../burger.png")} alt="" />
-                        <h3>Dupli  cheeseburge</h3>
-                        <p>06h-08h</p>
+                </nav>
+                <section className="Center">
+                    <div className="filter">
+                        <div className="active">
+                            <p>Topla Jela</p>
+                        </div>
+                        <div>
+                            <p>Topla Jela</p>
+                        </div>
+                        <div>
+                            <p>Topla Jela</p>
+                        </div>
+                        <div>
+                            <p>Topla Jela</p>
+                        </div>
+                        <div>
+                            <p>Topla Jela</p>
+                        </div>
                     </div>
-                    <div>
-                        <img src={require("../../burger.png")} alt="" />
-                        <h3>Dupli  cheeseburge</h3>
-                        <p>06h-08h</p>
+                    <h2>Odabir jela</h2>
+                    <div className="Food">
+                        <div>
+                            <img src={require("../../burger.png")} alt="" />
+                            <h3>Dupli  cheeseburge</h3>
+                            <p>06h-08h</p>
+                        </div>
+                        <div>
+                            <img src={require("../../burger.png")} alt="" />
+                            <h3>Dupli  cheeseburge</h3>
+                            <p>06h-08h</p>
+                        </div>
+                        <div>
+                            <img src={require("../../burger.png")} alt="" />
+                            <h3>Dupli  cheeseburge</h3>
+                            <p>06h-08h</p>
+                        </div>
+                        <div>
+                            <img src={require("../../burger.png")} alt="" />
+                            <h3>Burger</h3>
+                            <p>06h-08h</p>
+                        </div>
+                        <div>
+                            <img src={require("../../burger.png")} alt="" />
+                            <h3>Burger</h3>
+                            <p>06h-08h</p>
+                        </div>
+                        <div>
+                            <img src={require("../../burger.png")} alt="" />
+                            <h3>Burger</h3>
+                            <p>06h-08h</p>
+                        </div>
                     </div>
-                    <div>
-                        <img src={require("../../burger.png")} alt="" />
-                        <h3>Burger</h3>
-                        <p>06h-08h</p>
+                </section>
+                <div className="korpa">
+                    <h2>Narudzba #Anja002</h2>
+                    <div className="Helper">
+                        <p>Naruceno</p>
+                        <p>Kolicina</p>
                     </div>
-                    <div>
-                        <img src={require("../../burger.png")} alt="" />
-                        <h3>Burger</h3>
-                        <p>06h-08h</p>
+                    <hr />
+                    <div className="List">
+                        <div>
+                            <img src={require("../../burger.png")} alt="" />
+                            
+                                <h3>Dupli cheeseburge</h3>
+                        
+                            <span className="amount">3</span>
+                            <span className="remove">
+                                <img src={trash} alt="" />
+                            </span>
+                        </div>
+                        <div>
+                            <img src={require("../../burger.png")} alt="" />
+                            
+                                <h3>Dupli cheeseburge</h3>
+                        
+                            <span className="amount">3</span>
+                            <span className="remove">
+                                <img src={trash} alt="" />
+                            </span>
+                        </div>
+                        <div>
+                            <img src={require("../../burger.png")} alt="" />
+                            
+                                <h3>Dupli cheeseburge</h3>
+                        
+                            <span className="amount">3</span>
+                            <span className="remove">
+                                <img src={trash} alt="" />
+                            </span>
+                        </div>
+                        <div>
+                            <img src={require("../../burger.png")} alt="" />
+                            
+                                <h3>Dupli cheeseburge</h3>
+                        
+                            <span className="amount">3</span>
+                            <span className="remove">
+                                <img src={trash} alt="" />
+                            </span>
+                        </div>
+                        <button>Potvrdi</button>
                     </div>
-                    <div>
-                        <img src={require("../../burger.png")} alt="" />
-                        <h3>Burger</h3>
-                        <p>06h-08h</p>
-                    </div>
+
                 </div>
             </section>
         </>
